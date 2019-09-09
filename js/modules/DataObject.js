4b568bdda22146d87ea4b66e9450eb2fc2f343db
Yuliia Kandych <yuliiakandych@DIPLY-284669.fanshawec.ca>
2019-09-09 19:48:26 -0400

    js

---
+++ js/modules/DataObject.js
@@ -0,0 +1,13 @@
+//objects are colections of keys and values
+// remember to separate the keys and values with a comma
+//and that's about it
+//and use a colon between the key and the value
+
+const Person = {
+    name: "Trevor",
+    eyeColor: "blue",
+    hairColor: "red",
+    role: "Professor"
+}
+
+export default Person;
