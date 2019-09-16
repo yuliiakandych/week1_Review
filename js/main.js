1ddae1b6c379d146daf44b6efe914ed508e3495e
Yuliia Kandych <yuliiakandych@YuliiaD13818538.fanshawec.ca>
2019-09-16 19:47:46 -0400

    json, image, etc

--- js/main.js
+++ js/main.js
@@ -1,13 +1,42 @@
-import Person from "./modules/DataObject.js"
+import People from "./modules/DataObject.js"
 
 // IIFE - Immediately Invoced Function Expression
 
 (() => {
 
-console.log("fired!");
+console.log('fired!');
+//grab the bio container and ready to populate its content 
+const bioInfo = document.querySelector('.bio-wrapper').children;
 
-console.log (Person);
 
+
+
+function snowProfData(){
+    // change data on the page here
+    let currentProf = this.textContent;
+
+//this refers to the heading tag (the first child of the bio-wrapper div)
+bioInfo[0].textContent = People[currentProf].name;
+//this refers to the paragraph tag (the second child of the bio-wrapper div)
+bioInfo[1].textContent = People[currentProf].role;
+
+bioInfo[2].textContent = People[currentProf].bio;
+
+bioInfo[3].src = `images/${People[currentProf].avatar}`;
+
+    debugger;
+}
+
+for (let prof in People){
+    console.log(prof);
+
+
+    let teamButton = document.createElement('button');
+
+    teamButton.textContent = prof
+    teamButton.addEventListener('click', snowProfData);
+
+    document.querySelector('#section1').appendChild(teamButton);
+}
 debugger;
-
 })();
--- js/modules/DataObject.js
+++ js/modules/DataObject.js
@@ -3,11 +3,48 @@
 //and that's about it
 //and use a colon between the key and the value
 
-const Person = {
-    name: "Trevor",
-    eyeColor: "blue",
-    hairColor: "red",
-    role: "Professor"
+const Team = {
+    Trevor : {
+        name: "Trevor",
+        bio:"Happy Monday, Everybody",
+        eyeColor: "blue",
+        hairColor: "red",
+        role: "Professor",
+        avatar: "olaf.jpeg"  
+    },
+
+    Joe :
+    {
+        name: "Joe",
+        bio:"pampam",
+        eyeColor: "brown",
+        hairColor: "greyish",
+        role: "Professor",
+        avatar: "pooh.jpeg"  
+    },
+
+    Jarrod :
+    {
+        name: "Jarrod",
+        bio:"blabla",
+        eyeColor: "blue",
+        hairColor: "blonde",
+        role: "Professor",
+        avatar: "zoolander.jpg"  
+    },
+
+    Justin :
+    {
+        name: "Justin",
+        bio:"blabla",
+        eyeColor: "brown",
+        hairColor: "black",
+        role: "Professor",
+        avatar: "justin.jpeg"  
+    }
+  
+  
+  
 }
 
-export default Person;
+export default Team;
